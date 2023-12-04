ec3180128fdaa2d915e05f0f99cc7274f6906fe1
kevinphillips11 <150522296+kevinphillips11@users.noreply.github.com>
2023-12-04 18:23:02 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,27 @@
 from flask import Flask
+from datetime import datetime
+from pytz import timezone
 
 app = Flask(__name__)
 
 @app.route('/')
-def hello_world():
-    return 'Hello, World!'
+def greet_user():
+    # Set the timezone to New York
+    new_york_timezone = timezone('America/New_York')
+    current_time = datetime.now(new_york_timezone)
+    current_hour = current_time.hour
+
+    # Greet the user based on the current hour
+    if 5 <= current_hour < 12:
+        greeting = 'Good morning!'
+    elif 12 <= current_hour < 17:
+        greeting = 'Good afternoon!'
+    elif 17 <= current_hour < 21:
+        greeting = 'Good evening!'
+    else:
+        greeting = 'Good night!'
+
+    return f'{greeting} Welcome to the Flask app!'
 
 if __name__ == '__main__':
     app.run(debug=True)
